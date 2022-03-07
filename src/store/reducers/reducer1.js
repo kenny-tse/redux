19fa8808d21a83980ad23031d9309eac63f14c0e
Kenny Tse <kennytse91@gmail.com>
2022-03-07 12:42:44 -0800

    set up files for react-redux

---
+++ src/store/reducers/reducer1.js
@@ -0,0 +1,27 @@
+import * as ACTION_TYPES from '../actions/action_types '
+
+// this is the actual redux state
+const initalState = {
+  stateprop1: false
+}
+
+const rootReducers = (state = initalState, action) => {
+
+  switch (action.type) {
+
+    case ACTION_TYPES.SUCCESS:
+      return {
+        ...state,
+        stateprop1: true
+      }
+
+    case ACTION_TYPES.FAILURE:
+      return {
+        ...state,
+        stateprop1: false
+      }
+
+    default:
+      return state;
+  }
+}
